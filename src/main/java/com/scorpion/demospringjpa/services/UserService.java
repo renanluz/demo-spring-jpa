89650cfdec581163591a408152ea487ce799732f
Renan Luz <ren4n@live.com>
2021-10-05 00:30:33 -0300

    Service layer, component registration

---
+++ src/main/java/com/scorpion/demospringjpa/services/UserService.java
@@ -0,0 +1,26 @@
+package com.scorpion.demospringjpa.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.scorpion.demospringjpa.entities.User;
+import com.scorpion.demospringjpa.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
